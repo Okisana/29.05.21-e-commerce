96d35e263b874143cd29b4dc458227123b998e92
Oksana <77448138+Okisana@users.noreply.github.com>
2021-05-31 16:40:23 +0300

    id #

--- js/main.js
+++ js/main.js
@@ -29,20 +29,16 @@ blackDress = {
 
 $("#btnPlus1").click(function () {
   number1 += 1;
-  $(".quantity1").html(`<span class="quantity1">${number1}</span>`);
+  $("#quantity1").html(`<span class="quantity1">${number1}</span>`);
   price1 = (floralDress.price * number1).toFixed(2);
-  $(".totalOneProduct1").html(
-    `<span class="totalOneProduct1">${price1}</span>`
-  );
+  $("#totalOneProduct1").html(`<span id="totalOneProduct1">${price1}</span>`);
 });
 
-$(".btnPlus2").click(function () {
+$("#btnPlus2").click(function () {
   number2 += 1;
-  $(".quantity2").html(`<span class="quantity1">${number2}</span>`);
+  $("#quantity2").html(`<span class="quantity1">${number2}</span>`);
   price2 = (blackDress.price * number2).toFixed(2);
-  $(".totalOneProduct2").html(
-    `<span class="totalOneProduct2">${price2}</span>`
-  );
+  $("#totalOneProduct2").html(`<span id="totalOneProduct2">${price2}</span>`);
 });
 
 $("#btnMinus1").click(function () {
@@ -50,23 +46,19 @@ $("#btnMinus1").click(function () {
     return;
   }
   number1 -= 1;
-  $(".quantity1").html(`<span class="quantity1">${number1}</span>`);
+  $("#quantity1").html(`<span class="quantity1">${number1}</span>`);
   let price1 = (floralDress.price * number1).toFixed(2);
-  $(".totalOneProduct1").html(
-    `<span class="totalOneProduct1">${price1}</span>`
-  );
+  $("#totalOneProduct1").html(`<span id="totalOneProduct1">${price1}</span>`);
 });
 
-$(".btnMinus2").click(function () {
+$("#btnMinus2").click(function () {
   if (number2 <= 1) {
     return;
   }
   number2 -= 1;
-  $(".quantity2").html(`<span class="quantity1">${number2}</span>`);
+  $("#quantity2").html(`<span class="quantity1">${number2}</span>`);
   let price2 = (blackDress.price * number2).toFixed(2);
-  $(".totalOneProduct2").html(
-    `<span class="totalOneProduct2">${price2}</span>`
-  );
+  $("#totalOneProduct2").html(`<span id="totalOneProduct2">${price2}</span>`);
 });
 
 $(".remove1").click(function () {
